b0de8364ef810587c2ce8c951c7cc82a89450bdc
MohammedAfthab18 <afthab2282@gmail.com>
2024-09-30 22:03:28 -0700

    method chaining applied on index.php

--- src/Transaction.php
+++ src/Transaction.php
@@ -3,22 +3,33 @@
 declare(strict_types=1);
 
 class Transaction {
+
     // use type hint
     private float $amount;
     public string $description;
     public float $taxamount;
     public float $discountamount;
 
+    // using constructor - Magic Methods in PHP and destructor is also a Magic Method in PHP
+
     public function __construct(float $amount, string $description , float $taxamount , float $discountamount) {
         $this->amount = $amount;   // this.amount = amount
         $this->description = $description; // this.description = description
         $this->taxamount = $taxamount; // this.taxamount = taxamount 
         $this->discountamount = $discountamount; // this.discountamount = discountamount
+        echo "Transaction created: {$this->description}<br>";
     } 
+    // Destructor
+
+    public function __destruct() {
+        echo "Transaction destroyed: {$this->description}<br>";
+    }
+    // Accessing Private Variable in the Public Function -- Access Specifiers
 
     public function getAmount(): float{
         return $this->amount;
     }
+    // Simple Method 
 
     // public function addTax(float $rate) {
     //     $this->taxamount = $this->amount * $rate /100;  // this.amount = this.amount * rate / 100;
@@ -28,7 +39,7 @@ class Transaction {
     //     $this->discountamount = $this->amount * $rate /100;
     // }
 
-    // method chaining
+    // Method Chaining
      
     public function addTax(float $rate): self {
         $this->taxamount = $this->amount * $rate / 100;
--- src/public/index.php
+++ src/public/index.php
@@ -4,19 +4,38 @@ require_once '../Transaction.php';
 // Classes and Objects
 //  creating an object -- objectName = new className();
 
-$transaction = new Transaction( 255.9, 'Transaction 1' , 0.0 , 0.0);  // passing in the arguments 
+// Multiple Instances in the class
+$transaction1 = new Transaction( 255.9, 'Transaction 1' , 0.0 , 0.0);  // passing in the arguments 
 
-$price = $transaction
+$price1 = $transaction1
             ->addTax(7)
             ->getDiscount(3)
             ->calculateTotalAmount();   // Method Chaining
 
+$transaction2 = new Transaction( 783.4, 'Transaction 2' , 0.0 , 0.0);  // passing in the arguments 
+
+$price2 = $transaction2
+            ->addTax(5)
+            ->getDiscount(2)
+            ->calculateTotalAmount();   // Method Chaining
+
+            // Transaction 1
 echo '<pre>';
-echo 'After Tax and Discount, the total amount will be $' . $price;
+echo 'After Tax and Discount, the total amount will be $' . $price1;
 echo '</pre>';
 
 echo '<pre>';
-print_r($transaction);
+print_r($transaction1);
+echo '</pre>';
+
+            // Transaction 2     
+              
+echo '<pre>';
+echo 'After Tax and Discount, the total amount will be $' . $price2;
+echo '</pre>';
+
+echo '<pre>';
+print_r($transaction2);
 echo '</pre>';
 
 // echo($transaction->getAmount()); -- private variable amount is called in here using public function
